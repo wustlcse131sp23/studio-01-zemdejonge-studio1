9035849051895568ff605813717fa233b293b8fd
gabrielzem <gabrielzem@gabriels-mac.dhcp.wustl.edu>
2023-01-26 12:21:08 -0600

    done!

--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -10,8 +10,9 @@ public class HiOne {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
-		String name = in.nextLine();
+		System.out.println("What's your name?");
+		String name = in.next();
+		System.out.println("Hi " + name + ", how are you?");
 		//
 		// Below this line, enter code so that this program's output says
 		//      Hi, Pat.  How are you?
--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,9 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		boolean isOrdered = (x > y && y > z) || (z > y && y > x);
+		System.out.println(isOrdered);
+	
 
 	}
 
